1d26a23ce2d0f538a322d3f6026393f3969fff9c
raydevcp20 <rayanecristina.19@gmail.com>
2025-08-11 20:44:50 -0300

    Feat: código vistos para aula 14 e 15, angular material

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   redirectToDashboard() {
     this.router.navigate(['/dashboard']);
--- src/app/pages/pacientes/create/create.component.ts
+++ src/app/pages/pacientes/create/create.component.ts
@@ -1,72 +1,92 @@
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-create',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css',
 })
 export class CreateComponent {
-  text = new FormControl({value: "Rayane", disabled: false});
+  text = new FormControl({ value: 'Rayane', disabled: false });
   fb = inject(FormBuilder);
-  pacienteForm:FormGroup = this.fb.group({
-      nome: ["", [Validators.minLength(3), Validators.required]],
-      idade: ["", Validators.required],
-      endereco: this.fb.group({
-        rua: ["", Validators.required],
-        cidade: [""],
-        estado: [{value:"", disabled: true}]
-      }),
-      alergias: this.fb.array([])
-    });
+  pacienteForm: FormGroup = this.fb.group({
+    nome: ['', [Validators.minLength(3), Validators.required]],
+    idade: ['', Validators.required],
+    endereco: this.fb.group({
+      rua: ['', Validators.required],
+      cidade: [''],
+      estado: [{ value: '', disabled: true }],
+    }),
+    alergias: this.fb.array([]),
+  });
 
-  constructor(){
+  constructor() {
     // let idadeField = fb.control('');
-
-    // this.pacienteForm = 
+    // this.pacienteForm =
   }
 
-  validadorNome(control: any){
-    if(control.value === "Rayane"){
-      return { nomeInvalido: {atual: control.value, valorErrado: "Rayane" } };
+  validadorNome(control: any) {
+    if (control.value === 'Rayane') {
+      return { nomeInvalido: { atual: control.value, valorErrado: 'Rayane' } };
     }
     return null;
   }
-  
+
   ngOnInit() {
     // console.log("valor inicial: ", this.text.value);
-    console.log("alergias: ", this.alergias.value);
-    console.log("alergias controls: ", this.alergias.controls);
+    console.log('alergias: ', this.alergias.value);
+    console.log('alergias controls: ', this.alergias.controls);
   }
-  
-  clicou(){
-    if(this.pacienteForm.get('alergias')?.value.length === 0){
-      return alert("Você precisa adicionar pelo menos uma alergia!");
+
+  clicou() {
+    if (this.pacienteForm.get('alergias')?.value.length === 0) {
+      return alert('Você precisa adicionar pelo menos uma alergia!');
     }
-    console.log("Erros: ", this.pacienteForm.get('nome')?.errors);
-    console.log("depois que digitei: ", this.text.value);
-    console.log("formulário com formBuilder: ", this.pacienteForm.value);
-    console.log("alergias.value: ", this.alergias.value);
+    console.log('Erros: ', this.pacienteForm.get('nome')?.errors);
+    console.log('depois que digitei: ', this.text.value);
+    console.log('formulário com formBuilder: ', this.pacienteForm.value);
+    console.log('alergias.value: ', this.alergias.value);
   }
 
-  inputTeste(){
-    console.log("inputTeste: ", this.text.value);
+  inputTeste() {
+    console.log('inputTeste: ', this.text.value);
   }
 
-  get alergias(){
+  get alergias() {
     return this.pacienteForm.get('alergias') as FormArray;
   }
 
   removeAlergia(index: number) {
     this.alergias.removeAt(index);
-    console.log("alergias após remoção: ", this.alergias.value);
+    console.log('alergias após remoção: ', this.alergias.value);
   }
 
-  addAlergia(){
-    this.alergias.push(this.fb.group({
-      nome: ['', Validators.required],
-      descricao: ['', Validators.required]
-    }));
+  addAlergia() {
+    this.alergias.push(
+      this.fb.group({
+        nome: ['', Validators.required],
+        descricao: ['', Validators.required],
+      })
+    );
   }
 }
